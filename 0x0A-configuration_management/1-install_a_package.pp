abbf073bdc44eebe775544f9b72701dcac36792a
MoMaherB <maher1525@gmail.com>
2024-06-29 02:27:26 +0300

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #!/usr/bin/pup
 # Install flask version 2.1.0
-package {'flask':
+package { 'werkzeug':
+  ensure   => '1.0.1',
+  provider => 'pip3',
+}
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['werkzeug'],
 }
